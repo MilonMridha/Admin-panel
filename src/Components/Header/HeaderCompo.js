d042183f81be03335caf41eb44464d45fe2970ae
MilonMridha <milonmridha570@gmail.com>
2022-08-10 02:00:29 +0600

    admin panel dashboard

---
+++ src/Components/Header/HeaderCompo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const HeaderCompo = () => {
+    return (
+        <div>
+           <Navbar/>
+        </div>
+    );
+};
+
+export default HeaderCompo;
